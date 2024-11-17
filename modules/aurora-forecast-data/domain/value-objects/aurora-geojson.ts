35254a7dd13bee037ac43b8039578730b759251f
peggalex <alex.pegg@mail.utoronto.ca>
2024-11-17 15:15:03 -0500

    make geojson polygon closed-ring

--- modules/aurora-forecast-data/domain/value-objects/aurora-geojson.ts
+++ modules/aurora-forecast-data/domain/value-objects/aurora-geojson.ts
@@ -1,57 +1,57 @@
 import {
-	Feature,
-	FeatureCollection as GeoJsonFeatureCollection,
-	Polygon,
+  Feature,
+  FeatureCollection as GeoJsonFeatureCollection,
+  Polygon,
 } from "geojson";
 import { input, output, z } from "zod";
 
-interface IAuroraGeoFeature extends Feature {
-	type: "Feature";
-	id: string;
-	properties: {
-		aurora: number;
-		lat: number;
-		long: number;
-	};
-	geometry: Polygon;
+export interface IAuroraGeoFeature extends Feature {
+  type: "Feature";
+  id: string;
+  properties: {
+    aurora: number;
+    lat: number;
+    long: number;
+  };
+  geometry: Polygon;
 }
 
 /**
  * Extends GeoJson standard type (feature collection)
  */
 interface IAuroraGeoJson extends GeoJsonFeatureCollection {
-	type: "FeatureCollection";
-	features: IAuroraGeoFeature[];
+  type: "FeatureCollection";
+  features: IAuroraGeoFeature[];
 }
 
 const schema = z.object({
-	type: z.literal("FeatureCollection"),
-	features: z.array(
-		z.object({
-			type: z.literal("Feature"),
-			id: z.string(),
-			properties: z.object({
-				aurora: z.number(),
-				lat: z.number(),
-				long: z.number(),
-			}),
-			geometry: z.object({
-				type: z.literal("Polygon"),
-				// coordinates: [[[y1,x1], [y2,x2],...]]
-				coordinates: z
-					.array(
-						z.array(
-							z.tuple([
-								z.number().min(-180).max(180),
-								// range: [-180, 180]; different from NOAA data which is [0, 360]
-								z.number().min(-90).max(90),
-							])
-						)
-					)
-					.length(1),
-			}),
-		})
-	),
+  type: z.literal("FeatureCollection"),
+  features: z.array(
+    z.object({
+      type: z.literal("Feature"),
+      id: z.string(),
+      properties: z.object({
+        aurora: z.number(),
+        lat: z.number(),
+        long: z.number(),
+      }),
+      geometry: z.object({
+        type: z.literal("Polygon"),
+        // coordinates: [[[y1,x1], [y2,x2],...]]
+        coordinates: z
+          .array(
+            z.array(
+              z.tuple([
+                z.number().min(-180).max(180),
+                // range: [-180, 180]; different from NOAA data which is [0, 360]
+                z.number().min(-90).max(90),
+              ])
+            )
+          )
+          .length(1),
+      }),
+    })
+  ),
 });
 
 /**
@@ -59,14 +59,14 @@ const schema = z.object({
  * Coordinates with an aurora value of 0 are omitted to save space
  */
 export class AuroraGeoJson implements IAuroraGeoJson {
-	readonly type = "FeatureCollection";
-	declare readonly features: IAuroraGeoFeature[];
+  readonly type = "FeatureCollection";
+  declare readonly features: IAuroraGeoFeature[];
 
-	private constructor(props: output<typeof schema>) {
-		this.features = props.features;
-	}
+  private constructor(props: output<typeof schema>) {
+    this.features = props.features;
+  }
 
-	static fromRaw(inputValue: input<typeof schema>) {
-		return new AuroraGeoJson(schema.parse(inputValue));
-	}
+  static fromRaw(inputValue: input<typeof schema>) {
+    return new AuroraGeoJson(schema.parse(inputValue));
+  }
 }
--- modules/aurora-forecast-data/infrastructure/aurora-forecast-mongo-repo.ts
+++ modules/aurora-forecast-data/infrastructure/aurora-forecast-mongo-repo.ts
@@ -7,172 +7,173 @@ import { ObjectId, WithId } from "mongodb";
 import { logTime, logTimeSync } from "../../shared/log-time";
 
 export class AuroraForecastMongoRepository
-	extends MongoRepository<AuroraForecast, AuroraForecastMongoModel>
-	implements IAuroraForecastRepository
+  extends MongoRepository<AuroraForecast, AuroraForecastMongoModel>
+  implements IAuroraForecastRepository
 {
-	collectionName = "auroraForecasts";
+  collectionName = "auroraForecasts";
 
-	static async init(mongoConnectionStr: string) {
-		const repo = new AuroraForecastMongoRepository(mongoConnectionStr);
-		await repo.connectToDB((collection) =>
-			collection.createIndex({ forecastTime: 1 })
-		); // idempotent
-		return repo;
-	}
+  static async init(mongoConnectionStr: string) {
+    const repo = new AuroraForecastMongoRepository(mongoConnectionStr);
+    await repo.connectToDB(async (collection) => {
+      await collection.createIndex({ forecastTime: 1 });
+      await collection.createIndex({ geoJson: "2dsphere" });
+    }); // idempotent
+    return repo;
+  }
 
-	toDocument(auroraForecast: AuroraForecast): AuroraForecastMongoModel {
-		return {
-			id: auroraForecast.id,
-			forecastTime: auroraForecast.forecastTime,
-			observationTime: auroraForecast.observationTime,
-			geoJson: auroraForecast.geoJson,
-		};
-	}
+  toDocument(auroraForecast: AuroraForecast): AuroraForecastMongoModel {
+    return {
+      id: auroraForecast.id,
+      forecastTime: auroraForecast.forecastTime,
+      observationTime: auroraForecast.observationTime,
+      geoJson: auroraForecast.geoJson,
+    };
+  }
 
-	fromDocument(document: WithId<AuroraForecastMongoModel>): AuroraForecast {
-		const forecast = logTimeSync(
-			{
-				callback: () =>
-					new AuroraForecast(
-						document.id,
-						document.forecastTime,
-						document.observationTime,
-						document.geoJson
-					),
-				taskName: "generate aurora forecast",
-				context: { forecastTime: document.forecastTime },
-			},
-			1
-		);
-		return forecast;
-	}
+  fromDocument(document: WithId<AuroraForecastMongoModel>): AuroraForecast {
+    const forecast = logTimeSync(
+      {
+        callback: () =>
+          new AuroraForecast(
+            document.id,
+            document.forecastTime,
+            document.observationTime,
+            document.geoJson
+          ),
+        taskName: "generate aurora forecast",
+        context: { forecastTime: document.forecastTime },
+      },
+      1
+    );
+    return forecast;
+  }
 
-	async saveAuroraForecast(auroraForecast: AuroraForecast) {
-		await this.updateDocument(auroraForecast.id, auroraForecast);
-	}
+  async saveAuroraForecast(auroraForecast: AuroraForecast) {
+    await this.updateDocument(auroraForecast.id, auroraForecast);
+  }
 
-	async getLatestAuroraForecast(): Promise<AuroraForecast | null> {
-		return await this.getLatest();
-	}
+  async getLatestAuroraForecast(): Promise<AuroraForecast | null> {
+    return await this.getLatest();
+  }
 
-	async getMostCurrentAuroraForecast(): Promise<AuroraForecast | null> {
-		return this.connectToDB(async (collection) => {
-			const document = await collection.findOne(
-				{
-					forecastTime: { $lte: new Date() },
-				},
-				{
-					sort: {
-						_id: -1,
-					},
-				}
-			);
-			if (document === null) {
-				return null;
-			}
-			return this.fromDocument(document);
-		});
-	}
+  async getMostCurrentAuroraForecast(): Promise<AuroraForecast | null> {
+    return this.connectToDB(async (collection) => {
+      const document = await collection.findOne(
+        {
+          forecastTime: { $lte: new Date() },
+        },
+        {
+          sort: {
+            _id: -1,
+          },
+        }
+      );
+      if (document === null) {
+        return null;
+      }
+      return this.fromDocument(document);
+    });
+  }
 
-	async getForecastForLocation({
-		lat,
-		long,
-		startDate,
-	}: {
-		lat: number;
-		long: number;
-		startDate: Date;
-	}): Promise<
-		{
-			forecastTime: Date;
-			auroraChance: number;
-		}[]
-	> {
-		const forecast = await logTime({
-			callback: () =>
-				this.connectToDB(async (collection) => {
-					const forecast = collection.aggregate([
-						{
-							$match: {
-								forecastTime: { $gte: startDate },
-							},
-						},
-						{
-							$project: {
-								properties: "$geoJson.features.properties",
-								forecastTime: 1,
-							},
-						},
-						{
-							$unwind: {
-								path: "$properties",
-							},
-						},
-						{
-							$match: {
-								"properties.lat": {
-									$eq: lat,
-								},
-								"properties.long": {
-									$eq: long,
-								},
-							},
-						},
-						{
-							$project: {
-								forecastTime: 1,
-								auroraChance: "$properties.aurora",
-							},
-						},
-						{
-							$sort: {
-								forecastTime: 1,
-							},
-						},
-					]);
+  async getForecastForLocation({
+    lat,
+    long,
+    startDate,
+  }: {
+    lat: number;
+    long: number;
+    startDate: Date;
+  }): Promise<
+    {
+      forecastTime: Date;
+      auroraChance: number;
+    }[]
+  > {
+    const forecast = await logTime({
+      callback: () =>
+        this.connectToDB(async (collection) => {
+          const forecast = collection.aggregate([
+            {
+              $match: {
+                forecastTime: { $gte: startDate },
+              },
+            },
+            {
+              $project: {
+                properties: "$geoJson.features.properties",
+                forecastTime: 1,
+              },
+            },
+            {
+              $unwind: {
+                path: "$properties",
+              },
+            },
+            {
+              $match: {
+                "properties.lat": {
+                  $eq: lat,
+                },
+                "properties.long": {
+                  $eq: long,
+                },
+              },
+            },
+            {
+              $project: {
+                forecastTime: 1,
+                auroraChance: "$properties.aurora",
+              },
+            },
+            {
+              $sort: {
+                forecastTime: 1,
+              },
+            },
+          ]);
 
-					return (await forecast.toArray()) as {
-						forecastTime: Date;
-						auroraChance: number;
-					}[];
-				}),
-			taskName: "aggregation forecast query",
-			context: {},
-		});
-		return forecast;
-	}
+          return (await forecast.toArray()) as {
+            forecastTime: Date;
+            auroraChance: number;
+          }[];
+        }),
+      taskName: "aggregation forecast query",
+      context: {},
+    });
+    return forecast;
+  }
 
-	async getAuroraForecastsSinceDate(
-		startDate: Date
-	): Promise<AuroraForecast[]> {
-		return this.connectToDB(async (collection) => {
-			/*console.log(
+  async getAuroraForecastsSinceDate(
+    startDate: Date
+  ): Promise<AuroraForecast[]> {
+    return this.connectToDB(async (collection) => {
+      /*console.log(
 				`\t[${new Date().toLocaleString()}]fetching from mongo`
 			);*/
-			const cursor = collection.find(
-				{
-					forecastTime: { $gte: startDate },
-				},
-				{
-					sort: {
-						_id: 1,
-					},
-				}
-			);
-			const auroraForecasts: AuroraForecast[] = [];
-			while (await cursor.hasNext()) {
-				const doc = await logTime(
-					{
-						callback: () => cursor.next(),
-						taskName: "cursor.next",
-						context: {},
-					},
-					1
-				);
-				auroraForecasts.push(this.fromDocument(doc!));
-			}
-			//console.log(`\t[${new Date().toLocaleString()}]fetched from mongo`);
-			return auroraForecasts;
-		});
-	}
+      const cursor = collection.find(
+        {
+          forecastTime: { $gte: startDate },
+        },
+        {
+          sort: {
+            _id: 1,
+          },
+        }
+      );
+      const auroraForecasts: AuroraForecast[] = [];
+      while (await cursor.hasNext()) {
+        const doc = await logTime(
+          {
+            callback: () => cursor.next(),
+            taskName: "cursor.next",
+            context: {},
+          },
+          1
+        );
+        auroraForecasts.push(this.fromDocument(doc!));
+      }
+      //console.log(`\t[${new Date().toLocaleString()}]fetched from mongo`);
+      return auroraForecasts;
+    });
+  }
 }
