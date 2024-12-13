e8ee1d438f37c56977c2dff790b68ccac13de00b
peggalex <alex.pegg@mail.utoronto.ca>
2024-12-13 09:11:59 +0800

    docker - move mongo port, add weather api key

--- modules/api-gateway/module.ts
+++ modules/api-gateway/module.ts
@@ -8,154 +8,146 @@ import { AutoCompleteLocationSearchRequest } from "./domain/value-objects/auto-c
 import { GetAuroraChanceForLocationRequest } from "./domain/value-objects/get-aurora-chance-for-location-request";
 
 export class ApiGatewayModule {
-	private static singleton:
-		| { type: "NOT_INITIALIZED" }
-		| { type: "INITIALIZING" }
-		| { type: "INITIALIZED"; instance: ApiGatewayModule } = {
-		type: "NOT_INITIALIZED",
-	};
+  private static singleton:
+    | { type: "NOT_INITIALIZED" }
+    | { type: "INITIALIZING" }
+    | { type: "INITIALIZED"; instance: ApiGatewayModule } = {
+    type: "NOT_INITIALIZED",
+  };
 
-	private constructor(private app: Express) {}
+  private constructor(private app: Express) {}
 
-	/**
-	 * Initialize module instance
-	 */
-	static async init(
-		auroraForecastDataModule: IAuroraForecastDataModuleApi,
-		weatherForecastModule: IWeatherForecastModuleApi,
-		locationLookupModule: ILocationLookupModuleApi
-	) {
-		if (ApiGatewayModule.singleton.type !== "NOT_INITIALIZED") {
-			throw Error(
-				`Already started initializing AuroraForecastDataModule`
-			);
-		}
-		ApiGatewayModule.singleton = {
-			type: "INITIALIZING",
-		};
-		const instance = await ApiGatewayModule.createInstance(
-			auroraForecastDataModule,
-			weatherForecastModule,
-			locationLookupModule
-		);
-		ApiGatewayModule.singleton = {
-			type: "INITIALIZED",
-			instance,
-		};
-		return instance;
-	}
+  /**
+   * Initialize module instance
+   */
+  static async init(
+    auroraForecastDataModule: IAuroraForecastDataModuleApi,
+    weatherForecastModule: IWeatherForecastModuleApi,
+    locationLookupModule: ILocationLookupModuleApi
+  ) {
+    if (ApiGatewayModule.singleton.type !== "NOT_INITIALIZED") {
+      throw Error(`Already started initializing AuroraForecastDataModule`);
+    }
+    ApiGatewayModule.singleton = {
+      type: "INITIALIZING",
+    };
+    const instance = await ApiGatewayModule.createInstance(
+      auroraForecastDataModule,
+      weatherForecastModule,
+      locationLookupModule
+    );
+    ApiGatewayModule.singleton = {
+      type: "INITIALIZED",
+      instance,
+    };
+    return instance;
+  }
 
-	private static async createInstance(
-		auroraForecastDataModule: IAuroraForecastDataModuleApi,
-		weatherForecastModule: IWeatherForecastModuleApi,
-		locationLookupModule: ILocationLookupModuleApi
-	): Promise<ApiGatewayModule> {
-		const logger = {
-			info: (msg: string) =>
-				console.log(`[${new Date().toLocaleTimeString()}] ${msg}`),
-		};
+  private static async createInstance(
+    auroraForecastDataModule: IAuroraForecastDataModuleApi,
+    weatherForecastModule: IWeatherForecastModuleApi,
+    locationLookupModule: ILocationLookupModuleApi
+  ): Promise<ApiGatewayModule> {
+    const logger = {
+      info: (msg: string) =>
+        console.log(`[${new Date().toLocaleTimeString()}] ${msg}`),
+    };
 
-		const app: Express = express();
-		const port = 3000;
+    const app: Express = express();
+    const port = 3000;
 
-		app.use(cors());
+    app.use(cors());
 
-		app.get("/", (req: Request, res: Response) => {
-			res.send("Express + TypeScript Server");
-		});
+    app.get("/", (req: Request, res: Response) => {
+      res.send("Express + TypeScript Server");
+    });
 
-		app.get(
-			"/get-current-aurora-forecast",
-			async (req: Request, res: Response, next: NextFunction) => {
-				try {
-					const currentAuroraForecast =
-						await auroraForecastDataModule.getCurrentAuroraForecast();
-					res.json(currentAuroraForecast);
-					res.status(currentAuroraForecast === null ? 404 : 200);
-				} catch (e) {
-					next(e);
-				}
-			}
-		);
+    app.get(
+      "/get-current-aurora-forecast",
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const currentAuroraForecast =
+            await auroraForecastDataModule.getCurrentAuroraForecast();
+          res.json(currentAuroraForecast);
+          res.status(currentAuroraForecast === null ? 404 : 200);
+        } catch (e) {
+          next(e);
+        }
+      }
+    );
 
-		app.get(
-			"/get-current-weather-forecast",
-			async (req: Request, res: Response, next: NextFunction) => {
-				try {
-					console.log(req.query);
-					const request = GetCurrentWeatherForecastRequest.fromRaw(
-						req.query as any
-					);
-					const currentWeatherForecast =
-						await weatherForecastModule.getCurrentWeatherForecast({
-							longitude: request.longitude,
-							latitude: request.latitude,
-						});
-					res.json(currentWeatherForecast);
-					res.status(200);
-				} catch (e) {
-					next(e);
-				}
-			}
-		);
+    app.get(
+      "/get-current-weather-forecast",
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          console.log(req.query);
+          const request = GetCurrentWeatherForecastRequest.fromRaw(
+            req.query as any
+          );
+          const currentWeatherForecast =
+            await weatherForecastModule.getCurrentWeatherForecast({
+              longitude: request.longitude,
+              latitude: request.latitude,
+            });
+          res.json(currentWeatherForecast);
+          res.status(200);
+        } catch (e) {
+          next(e);
+        }
+      }
+    );
 
-		app.get(
-			"/auto-complete-location-search",
-			async (req: Request, res: Response, next: NextFunction) => {
-				try {
-					console.log(req.query);
-					const request = AutoCompleteLocationSearchRequest.fromRaw(
-						req.query as any
-					);
-					const autoCompleteLocations =
-						await locationLookupModule.autoCompleteLocationSearch({
-							searchTerm: request.searchTerm,
-						});
-					res.json(autoCompleteLocations);
-					res.status(200);
-				} catch (e) {
-					next(e);
-				}
-			}
-		);
+    app.get(
+      "/auto-complete-location-search",
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          console.log(req.query);
+          const request = AutoCompleteLocationSearchRequest.fromRaw(
+            req.query as any
+          );
+          const autoCompleteLocations =
+            await locationLookupModule.autoCompleteLocationSearch({
+              searchTerm: request.searchTerm,
+            });
+          res.json(autoCompleteLocations);
+          res.status(200);
+        } catch (e) {
+          next(e);
+        }
+      }
+    );
 
-		app.get(
-			"/aurora-chance-for-location",
-			async (req: Request, res: Response, next: NextFunction) => {
-				try {
-					console.log(req.query);
-					const request = GetAuroraChanceForLocationRequest.fromRaw(
-						req.query as any
-					);
-					const auroraForecastForLocation =
-						await auroraForecastDataModule.getAuroraForecastForLocation(
-							{
-								lat: request.lat,
-								long: request.long,
-								startDate: request.timestamp.valueOf(),
-							}
-						);
-					res.json(auroraForecastForLocation);
-					res.status(200);
-				} catch (e) {
-					next(e);
-				}
-			}
-		);
+    app.get(
+      "/aurora-chance-for-location",
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          console.log(req.query);
+          const request = GetAuroraChanceForLocationRequest.fromRaw(
+            req.query as any
+          );
+          const auroraForecastForLocation =
+            await auroraForecastDataModule.getAuroraForecastForLocation({
+              lat: request.lat,
+              long: request.long,
+              startDate: request.timestamp.valueOf(),
+            });
+          res.json(auroraForecastForLocation);
+          res.status(200);
+        } catch (e) {
+          next(e);
+        }
+      }
+    );
 
-		app.use(
-			(err: Error, req: Request, res: Response, next: NextFunction) => {
-				console.log("got error");
-				console.error(err.stack);
-				res.status(500).send("Something broke!");
-			}
-		);
-		app.listen(port, () => {
-			console.log(
-				`[server]: Server is running at http://localhost:${port}`
-			);
-		});
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      console.log("got error");
+      console.error(err.stack);
+      res.status(500).send("Something broke!");
+    });
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
 
-		return new ApiGatewayModule(app);
-	}
+    return new ApiGatewayModule(app);
+  }
 }
--- modules/aurora-forecast-data/infrastructure/aurora-forecast-mongo-repo.ts
+++ modules/aurora-forecast-data/infrastructure/aurora-forecast-mongo-repo.ts
@@ -3,8 +3,8 @@ import { IAuroraForecastRepository } from "../domain/aurora-forecast-repository"
 import { IAuroraGeoFeature } from "../domain/value-objects/aurora-geojson";
 import { AuroraForecast } from "../domain/aurora-forecast";
 import {
-	AuroraForecastMongoModel,
-	LongLatStr,
+  AuroraForecastMongoModel,
+  LongLatStr,
 } from "./models/aurora-forecast-mongo-model";
 import { WithId } from "mongodb";
 import { logTime, logTimeSync } from "../../shared/log-time";
@@ -12,200 +12,200 @@ import { objectFromEntriesTyped } from "../../shared/object-from-entries-typed";
 import { GeoJsonService } from "../domain/services/geojson-service";
 
 const featureToLongLatStr = (feature: IAuroraGeoFeature): LongLatStr =>
-	createLongLatStr(feature.properties);
+  createLongLatStr(feature.properties);
 
 const createLongLatStr = ({
-	long,
-	lat,
+  long,
+  lat,
 }: {
-	long: number;
-	lat: number;
+  long: number;
+  lat: number;
 }): LongLatStr => `${Math.round(long)},${Math.round(lat)}`;
 
 const parseLongLatStr = (str: LongLatStr) => {
-	const [longStr, latStr] = str.split(",");
-	return { long: Number(longStr), lat: Number(latStr) };
+  const [longStr, latStr] = str.split(",");
+  return { long: Number(longStr), lat: Number(latStr) };
 };
 
 export class AuroraForecastMongoRepository
-	extends MongoRepository<AuroraForecast, AuroraForecastMongoModel>
-	implements IAuroraForecastRepository
+  extends MongoRepository<AuroraForecast, AuroraForecastMongoModel>
+  implements IAuroraForecastRepository
 {
-	collectionName = "auroraForecasts";
+  collectionName = "auroraForecasts";
 
-	static async init(mongoConnectionStr: string) {
-		const repo = new AuroraForecastMongoRepository(mongoConnectionStr);
-		await repo.connectToDB(async (collection) => {
-			await collection.createIndex({ forecastTime: 1 });
-		}); // idempotent
-		return repo;
-	}
+  static async init(mongoConnectionStr: string) {
+    const repo = new AuroraForecastMongoRepository(mongoConnectionStr);
+    await repo.connectToDB(async (collection) => {
+      await collection.createIndex({ forecastTime: 1 });
+    }); // idempotent
+    return repo;
+  }
 
-	toDocument(auroraForecast: AuroraForecast): AuroraForecastMongoModel {
-		return {
-			id: auroraForecast.id,
-			forecastTime: auroraForecast.forecastTime,
-			observationTime: auroraForecast.observationTime,
-			coordinateToAuroraChance: objectFromEntriesTyped(
-				auroraForecast.geoJson.features.map((f) => [
-					featureToLongLatStr(f),
-					f.properties.aurora,
-				])
-			),
-		};
-	}
+  toDocument(auroraForecast: AuroraForecast): AuroraForecastMongoModel {
+    return {
+      id: auroraForecast.id,
+      forecastTime: auroraForecast.forecastTime,
+      observationTime: auroraForecast.observationTime,
+      coordinateToAuroraChance: objectFromEntriesTyped(
+        auroraForecast.geoJson.features.map((f) => [
+          featureToLongLatStr(f),
+          f.properties.aurora,
+        ])
+      ),
+    };
+  }
 
-	fromDocument(document: WithId<AuroraForecastMongoModel>): AuroraForecast {
-		const forecast = logTimeSync(
-			{
-				callback: () => {
-					const features = Object.entries(
-						document.coordinateToAuroraChance
-					).map(([longLatStr, aurora], index): IAuroraGeoFeature => {
-						const { long, lat } = parseLongLatStr(
-							longLatStr as LongLatStr
-						);
-						return new GeoJsonService().getFeatureFromCoord({
-							index,
-							long,
-							lat,
-							aurora,
-						});
-					});
+  fromDocument(document: WithId<AuroraForecastMongoModel>): AuroraForecast {
+    const forecast = logTimeSync(
+      {
+        callback: () => {
+          const features = Object.entries(
+            document.coordinateToAuroraChance
+          ).map(([longLatStr, aurora], index): IAuroraGeoFeature => {
+            const { long, lat } = parseLongLatStr(longLatStr as LongLatStr);
+            return new GeoJsonService().getFeatureFromCoord({
+              index,
+              long,
+              lat,
+              aurora,
+            });
+          });
 
-					return new AuroraForecast(
-						document.id,
-						document.forecastTime,
-						document.observationTime,
-						{
-							type: "FeatureCollection",
-							features,
-						}
-					);
-				},
-				taskName: "generate aurora forecast",
-				context: { forecastTime: document.forecastTime },
-			},
-			1
-		);
-		return forecast;
-	}
+          return new AuroraForecast(
+            document.id,
+            document.forecastTime,
+            document.observationTime,
+            {
+              type: "FeatureCollection",
+              features,
+            }
+          );
+        },
+        taskName: "generate aurora forecast",
+        context: { forecastTime: document.forecastTime },
+      },
+      1
+    );
+    return forecast;
+  }
 
-	async saveAuroraForecast(auroraForecast: AuroraForecast) {
-		await this.updateDocument(auroraForecast.id, auroraForecast);
-	}
+  async saveAuroraForecast(auroraForecast: AuroraForecast) {
+    await this.updateDocument(auroraForecast.id, auroraForecast);
+  }
 
-	async getLatestAuroraForecast(): Promise<AuroraForecast | null> {
-		return await this.getLatest();
-	}
+  async getLatestAuroraForecast(): Promise<AuroraForecast | null> {
+    return await this.getLatest();
+  }
 
-	async getMostCurrentAuroraForecast(): Promise<AuroraForecast | null> {
-		return this.connectToDB(async (collection) => {
-			const [document] = await collection
-				.find({ forecastTime: { $lte: new Date() } })
-				.sort({ _id: -1 })
-				.limit(1)
-				.toArray();
-			if (!document) {
-				return null;
-			}
-			return this.fromDocument(document);
-		});
-	}
+  async getMostCurrentAuroraForecast(): Promise<AuroraForecast | null> {
+    return this.connectToDB(async (collection) => {
+      const [document] = await collection
+        .find({ forecastTime: { $lte: new Date() } })
+        .sort({ _id: -1 })
+        .limit(1)
+        .toArray();
+      if (!document) {
+        return null;
+      }
+      return this.fromDocument(document);
+    });
+  }
 
-	async getForecastForLocation({
-		lat,
-		long,
-		startDate,
-	}: {
-		lat: number;
-		long: number;
-		startDate: Date;
-	}): Promise<
-		{
-			forecastTime: Date;
-			auroraChance: number;
-		}[]
-	> {
-		const forecast = await logTime({
-			callback: () =>
-				this.connectToDB(async (collection) => {
-					const forecast = collection.aggregate([
-						{
-							$match: {
-								forecastTime: { $gte: startDate },
-							},
-						},
-						{
-							$project: {
-								forecastTime: 1,
-								auroraChance: {
-									$ifNull: [
-										{
-											$getField: {
-												field: {
-													$literal: createLongLatStr({
-														long,
-														lat,
-													}),
-												},
-												input: "$coordinateToAuroraChance",
-											},
-										},
-										0,
-									],
-								},
-							},
-						},
-						{
-							$sort: {
-								forecastTime: 1,
-							},
-						},
-					]);
+  async getForecastForLocation({
+    lat,
+    long,
+    startDate,
+  }: {
+    lat: number;
+    long: number;
+    startDate: Date;
+  }): Promise<
+    {
+      forecastTime: Date;
+      auroraChance: number;
+    }[]
+  > {
+    const forecast = await logTime({
+      callback: () =>
+        this.connectToDB(async (collection) => {
+          const forecast = collection.aggregate([
+            {
+              $match: {
+                forecastTime: { $gte: startDate },
+              },
+            },
+            {
+              $project: {
+                forecastTime: 1,
+                auroraChance: {
+                  $ifNull: [
+                    {
+                      $getField: {
+                        field: {
+                          $literal: createLongLatStr({
+                            long,
+                            lat,
+                          }),
+                        },
+                        input: "$coordinateToAuroraChance",
+                      },
+                    },
+                    0,
+                  ],
+                },
+              },
+            },
+            {
+              $sort: {
+                forecastTime: 1,
+              },
+            },
+          ]);
 
-					return (await forecast.toArray()) as {
-						forecastTime: Date;
-						auroraChance: number;
-					}[];
-				}),
-			taskName: "aggregation forecast query",
-			context: {},
-		});
-		return forecast;
-	}
+          return (await forecast.toArray()) as {
+            forecastTime: Date;
+            auroraChance: number;
+          }[];
+        }),
+      taskName: "aggregation forecast query",
+      context: {},
+    });
+    return forecast;
+  }
 
-	async getAuroraForecastsSinceDate(
-		startDate: Date
-	): Promise<AuroraForecast[]> {
-		return this.connectToDB(async (collection) => {
-			/*console.log(
+  async getAuroraForecastsSinceDate(
+    startDate: Date
+  ): Promise<AuroraForecast[]> {
+    return this.connectToDB(async (collection) => {
+      /*console.log(
 				`\t[${new Date().toLocaleString()}]fetching from mongo`
 			);*/
-			const cursor = collection.find(
-				{
-					forecastTime: { $gte: startDate },
-				},
-				{
-					sort: {
-						_id: 1,
-					},
-				}
-			);
-			const auroraForecasts: AuroraForecast[] = [];
-			while (await cursor.hasNext()) {
-				const doc = await logTime(
-					{
-						callback: () => cursor.next(),
-						taskName: "cursor.next",
-						context: {},
-					},
-					1
-				);
-				auroraForecasts.push(this.fromDocument(doc!));
-			}
-			//console.log(`\t[${new Date().toLocaleString()}]fetched from mongo`);
-			return auroraForecasts;
-		});
-	}
+      const cursor = collection.find(
+        {
+          forecastTime: { $gte: startDate },
+        },
+        {
+          sort: {
+            _id: 1,
+          },
+        }
+      );
+      const auroraForecasts: AuroraForecast[] = [];
+      while (await cursor.hasNext()) {
+        const doc = await logTime(
+          {
+            callback: () => cursor.next(),
+            taskName: "cursor.next",
+            context: {},
+          },
+          1
+        );
+        if (doc) {
+          auroraForecasts.push(this.fromDocument(doc));
+        }
+      }
+      //console.log(`\t[${new Date().toLocaleString()}]fetched from mongo`);
+      return auroraForecasts;
+    });
+  }
 }
